9638aa55b9816ec581f36b3808ce52b398f62a39
usausa <machi.pon@gmail.com>
2016-09-25 20:28:40 +0900

    Add Smart.Forms.Reactive

---
+++ Smart.Forms/Forms/ViewModels/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿namespace Smart.Forms.ViewModels
+{
+    using Smart.ComponentModel;
+    using Smart.Forms.Messaging;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public abstract class ViewModelBase : NotificationObject
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        private Messenger messenger;
+
+        /// <summary>
+        ///
+        /// </summary>
+        public Messenger Messenger
+        {
+            get { return messenger ?? (messenger = new Messenger()); }
+        }
+    }
+}
