b6391058e6731eca9dd06017929071e0b43bc0d4
usausa <machi.pon@gmail.com>
2017-01-20 16:02:37 +0900

    Add behaviors

---
+++ Smart.Forms/Forms/Interactivity/BindingContextDisposeBehavior.cs
@@ -0,0 +1,46 @@
+﻿namespace Smart.Forms.Interactivity
+{
+    using System;
+
+    using Xamarin.Forms;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public class BindingContextDisposeBehavior : BehaviorBase<NavigationPage>
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="bindable"></param>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Ignore")]
+        protected override void OnAttachedTo(NavigationPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            bindable.Popped += OnPopped;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="bindable"></param>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Ignore")]
+        protected override void OnDetachingFrom(NavigationPage bindable)
+        {
+            bindable.Popped -= OnPopped;
+
+            base.OnDetachingFrom(bindable);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnPopped(object sender, NavigationEventArgs e)
+        {
+            (e.Page.BindingContext as IDisposable)?.Dispose();
+        }
+    }
+}
---
+++ Smart.Forms/Forms/Interactivity/SetFocusMessageAction.cs
@@ -0,0 +1,21 @@
+﻿namespace Smart.Forms.Interactivity
+{
+    using Xamarin.Forms;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public class SetFocusMessageAction : MessageAction<VisualElement>
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="associatedObject"></param>
+        /// <param name="parameter"></param>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Ignore")]
+        protected override void Invoke(VisualElement associatedObject, object parameter)
+        {
+            associatedObject.Focus();
+        }
+    }
+}
