0cc51103dd9e77ad9c8323b47644c9cb9e7a1ffe
usausa <machi.pon@gmail.com>
2017-10-13 13:00:41 +0900

    Restore FormatConverter

---
+++ Smart.Forms/Forms/Data/FormatConverter.cs
@@ -0,0 +1,48 @@
+﻿namespace Smart.Forms.Data
+{
+    using System;
+    using System.Globalization;
+    using Xamarin.Forms;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public class FormatConverter : IValueConverter
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        public string Format { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return string.Empty;
+            }
+
+            return String.Format(culture, Format, value);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
