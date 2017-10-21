9c4da67a7ceeee0a35320a53ca50391b3b9916f9
usausa <machi.pon@gmail.com>
2017-10-21 19:59:05 +0900

    Add extensions

---
+++ Smart.Forms/ElementExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Smart
+{
+    using Xamarin.Forms;
+
+    public static class ElementExtensions
+    {
+        public static Page GetPage(this Element element)
+        {
+            while (element != null)
+            {
+                if (element is Page page)
+                {
+                    return page;
+                }
+
+                element = element.Parent;
+            }
+
+            return null;
+        }
+    }
+}
