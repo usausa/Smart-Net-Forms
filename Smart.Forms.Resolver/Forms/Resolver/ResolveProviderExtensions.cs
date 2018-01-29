4937605877070031739bd9b2cef1331e419d8505
usausa <machi.pon@gmail.com>
2018-01-29 16:54:57 +0900

    Add resolver project

---
+++ Smart.Forms.Resolver/Forms/Resolver/ResolveProviderExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Smart.Forms.Resolver
+{
+    using Smart.Resolver;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public static class ResolveProviderExtensions
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="provider"></param>
+        /// <param name="resolver"></param>
+        public static void UseSmartResolver(this ResolveProvider provider, SmartResolver resolver)
+        {
+            provider.Provider = new SmartResolveProvider(resolver);
+        }
+    }
+}
---
+++ Smart.Forms.Resolver/Forms/Resolver/SmartResolveProvider.cs
@@ -0,0 +1,33 @@
+﻿namespace Smart.Forms.Resolver
+{
+    using System;
+
+    using Smart.Resolver;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public class SmartResolveProvider : IResolveProvider
+    {
+        private readonly SmartResolver resolver;
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="resolver"></param>
+        public SmartResolveProvider(SmartResolver resolver)
+        {
+            this.resolver = resolver;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public object Resolve(Type type)
+        {
+            return resolver.Get(type);
+        }
+    }
+}
