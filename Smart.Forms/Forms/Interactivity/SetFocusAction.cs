bd9952054fa58a4bed6255f1050e71730dad6d6b
usausa <machi.pon@gmail.com>
2018-02-13 17:48:59 +0900

    Update messenger

--- Smart.Forms/Forms/Interactivity/SetFocusAction.cs
+++ Smart.Forms/Forms/Interactivity/SetFocusAction.cs
@@ -5,17 +5,33 @@
     /// <summary>
     ///
     /// </summary>
-    public sealed class SetFocusAction : TriggerAction<VisualElement>
+    public sealed class SetFocusAction : TriggerAction<BindableObject>
     {
+        /// <summary>
+        ///
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2104:DoNotDeclareReadOnlyMutableReferenceTypes", Justification = "BindableProperty")]
+        public static readonly BindableProperty TargetProperty = BindableProperty.Create(
+            nameof(Target),
+            typeof(VisualElement),
+            typeof(SetFocusAction));
+
+        public VisualElement Target
+        {
+            get => (VisualElement)GetValue(TargetProperty);
+            set => SetValue(TargetProperty, value);
+        }
+
         /// <summary>
         ///
         /// </summary>
         /// <param name="associatedObject"></param>
         /// <param name="parameter"></param>
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Ignore")]
-        protected override void Invoke(VisualElement associatedObject, object parameter)
+        protected override void Invoke(BindableObject associatedObject, object parameter)
         {
-            associatedObject.Focus();
+            var element = Target ?? (associatedObject as VisualElement);
+            element?.Focus();
         }
     }
 }
