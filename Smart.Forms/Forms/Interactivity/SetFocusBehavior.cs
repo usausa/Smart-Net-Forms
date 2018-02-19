ce9ff9fcdbeff176b9b09ac9aa3b32ea3a889170
usausa <machi.pon@gmail.com>
2018-02-19 11:35:55 +0900

    Add set focus behavior

---
+++ Smart.Forms/Forms/Interactivity/SetFocusBehavior.cs
@@ -0,0 +1,21 @@
+﻿namespace Smart.Forms.Interactivity
+{
+    using Xamarin.Forms;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public class SetFocusBehavior : BehaviorBase<VisualElement>
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="bindable"></param>
+        protected override void OnAttachedTo(VisualElement bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            Device.BeginInvokeOnMainThread(() => bindable.Focus());
+        }
+    }
+}
