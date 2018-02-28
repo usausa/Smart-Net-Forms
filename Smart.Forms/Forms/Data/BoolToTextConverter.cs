17cdc448838e58924eec1e531922209af466a75c
usausa <machi.pon@gmail.com>
2018-02-28 10:05:54 +0900

    Add text converter

---
+++ Smart.Forms/Forms/Data/BoolToTextConverter.cs
@@ -0,0 +1,49 @@
+﻿namespace Smart.Forms.Data
+{
+    using System;
+    using System.Globalization;
+
+    using Xamarin.Forms;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public sealed class BoolToTextConverter : IValueConverter
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        public string TrueText { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public string FalseText { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null && (bool)value ? TrueText : FalseText;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Equals(value, TrueText);
+        }
+    }
+}
