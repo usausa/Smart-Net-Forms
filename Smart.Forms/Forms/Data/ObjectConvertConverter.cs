8afa96e17e685c6826ba36ccc38615d748b05431
usausa <machi.pon@gmail.com>
2018-11-28 16:19:33 +0900

    Add object convert converter

---
+++ Smart.Forms/Forms/Data/ObjectConvertConverter.cs
@@ -0,0 +1,24 @@
+﻿namespace Smart.Forms.Data
+{
+    using System;
+    using System.Globalization;
+
+    using Smart.Converter;
+
+    using Xamarin.Forms;
+
+    public sealed class ObjectConvertConverter : IValueConverter
+    {
+        public IObjectConverter Converter { get; set; } = ObjectConverter.Default;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Converter.Convert(value, targetType);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Converter.Convert(value, targetType);
+        }
+    }
+}
