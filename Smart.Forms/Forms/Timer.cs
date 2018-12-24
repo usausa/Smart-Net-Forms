e004c6cdbf077033c7d7b6ca49f8c3cc70af0a43
usausa <machi.pon@gmail.com>
2018-12-24 19:40:20 +0900

    Add timer

---
+++ Smart.Forms/Forms/Timer.cs
@@ -0,0 +1,44 @@
+﻿namespace Smart.Forms
+{
+    using System;
+    using System.Threading;
+
+    using Xamarin.Forms;
+
+    public sealed class Timer
+    {
+        private readonly TimeSpan dueTime;
+
+        private readonly Action callback;
+
+        private int running;
+
+        public Timer(TimeSpan dueTime, Action callback)
+        {
+            this.dueTime = dueTime;
+            this.callback = callback;
+        }
+
+        public void Start()
+        {
+            if (Interlocked.Exchange(ref running, 1) == 0)
+            {
+                Device.StartTimer(dueTime, () =>
+                {
+                    var state = Interlocked.Exchange(ref running, running) == 1;
+                    if (state)
+                    {
+                        callback();
+                    }
+
+                    return state;
+                });
+            }
+        }
+
+        public void Stop()
+        {
+            Interlocked.Exchange(ref running, 0);
+        }
+    }
+}
