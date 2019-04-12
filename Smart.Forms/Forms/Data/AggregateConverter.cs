ae14134f488b2ac9a1ae1342d490bd5f4e9860d1
usausa <machi.pon@gmail.com>
2019-04-12 16:55:30 +0900

    Update converter

--- Smart.Forms/Forms/Data/AggregateConverter.cs
+++ Smart.Forms/Forms/Data/AggregateConverter.cs
@@ -1,22 +1,26 @@
-﻿namespace Smart.Forms.Data
+namespace Smart.Forms.Data
 {
     using System;
     using System.Collections.Generic;
+    using System.Collections.ObjectModel;
     using System.Globalization;
     using System.Linq;
 
     using Xamarin.Forms;
 
-    public sealed class AggregateConverter : List<IValueConverter>, IValueConverter
+    [ContentProperty("Converters")]
+    public class AggregateConverter : IValueConverter
     {
+        public Collection<IValueConverter> Converters { get; } = new Collection<IValueConverter>(new List<IValueConverter>());
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return this.Aggregate(value, (current, converter) => converter.Convert(current, targetType, parameter, culture));
+            return Converters.Aggregate(value, (current, converter) => converter.Convert(current, targetType, parameter, culture));
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotSupportedException();
+            return Converters.Reverse().Aggregate(value, (current, converter) => converter?.ConvertBack(current, targetType, parameter, culture));
         }
     }
 }
