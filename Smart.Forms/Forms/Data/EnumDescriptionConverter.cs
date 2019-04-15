8ad8c0d351f5ecb618ce16308f602c78837f9984
usausa <machi.pon@gmail.com>
2019-04-15 16:17:15 +0900

    Update converter

---
+++ Smart.Forms/Forms/Data/EnumDescriptionConverter.cs
@@ -0,0 +1,41 @@
+namespace Smart.Forms.Data
+{
+    using System;
+    using System.ComponentModel;
+    using System.Globalization;
+    using System.Reflection;
+
+    using Xamarin.Forms;
+
+    public sealed class EnumDescriptionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            if (value is Enum)
+            {
+                var type = value.GetType();
+                var mis = type.GetMember(value.ToString());
+                if (mis.Length > 0)
+                {
+                    var attr = mis[0].GetCustomAttribute<DescriptionAttribute>();
+                    if (attr != null)
+                    {
+                        return attr.Description;
+                    }
+                }
+            }
+
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
