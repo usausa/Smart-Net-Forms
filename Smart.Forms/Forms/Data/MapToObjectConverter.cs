bbb0b541ba6890bdb5fb114556c01bdaa4a47131
usausa <machi.pon@gmail.com>
2019-06-18 13:07:44 +0900

    Add converter

---
+++ Smart.Forms/Forms/Data/MapToObjectConverter.cs
@@ -0,0 +1,62 @@
+namespace Smart.Forms.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Globalization;
+    using System.Linq;
+
+    using Xamarin.Forms;
+
+    public class MapEntry<T>
+    {
+        public object Key { get; set; }
+
+        public T Value { get; set; }
+    }
+
+    public sealed class MapToColorEntry : MapEntry<Color>
+    {
+    }
+
+    public class MapToObjectConverter<T> : IValueConverter
+    {
+        public Collection<MapEntry<T>> Entries { get; } = new Collection<MapEntry<T>>(new List<MapEntry<T>>());
+
+        public T DefaultValue { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                if (value is IComparable comparable)
+                {
+                    var entry = Entries.FirstOrDefault(x => comparable.CompareTo(x.Key) == 0);
+                    if (entry != null)
+                    {
+                        return entry.Value;
+                    }
+                }
+                else
+                {
+                    var entry = Entries.FirstOrDefault(x => Equals(value, x.Key));
+                    if (entry != null)
+                    {
+                        return entry.Value;
+                    }
+                }
+            }
+
+            return DefaultValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    public sealed class MapToColorConverter : MapToObjectConverter<Color>
+    {
+    }
+}
