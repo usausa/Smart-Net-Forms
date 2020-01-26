ec3e17ec382524d5c6407a9a058b306dd99a8b1b
usausa <machi.pon@gmail.com>
2020-01-26 21:24:50 +0900

    Update resolver provider

---
+++ Smart.Forms.Resolver/Forms/Resolver/ServiceContainerResolverProvider.cs
@@ -0,0 +1,16 @@
+namespace Smart.Forms.Resolver
+{
+    using System;
+
+    public sealed class ServiceContainerResolverProvider : IResolveProvider
+    {
+        private readonly IServiceProvider provider;
+
+        public ServiceContainerResolverProvider(IServiceProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        public object Resolve(Type type) => provider.GetService(type);
+    }
+}
