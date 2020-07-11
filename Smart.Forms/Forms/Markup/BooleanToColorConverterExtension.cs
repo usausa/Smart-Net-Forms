03ac7f26938283edeee553717d170d85b87a75d0
usausa <machi.pon@gmail.com>
2020-07-11 19:08:38 +0900

    Add markup

---
+++ Smart.Forms/Forms/Markup/BooleanToColorConverterExtension.cs
@@ -0,0 +1,26 @@
+namespace Smart.Forms.Markup
+{
+    using System;
+
+    using Smart.Forms.Data;
+
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    public sealed class BooleanToColorConverterExtension : IMarkupExtension<BooleanToObjectConverter<Color>>
+    {
+        public Color True { get; set; }
+
+        public Color False { get; set; }
+
+        public BooleanToObjectConverter<Color> ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new BooleanToObjectConverter<Color> { TrueValue = True, FalseValue = False };
+        }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        {
+            return ProvideValue(serviceProvider);
+        }
+    }
+}
---
+++ Smart.Forms/Forms/Markup/BooleanToTextConverterExtension.cs
@@ -0,0 +1,25 @@
+namespace Smart.Forms.Markup
+{
+    using System;
+
+    using Smart.Forms.Data;
+
+    using Xamarin.Forms.Xaml;
+
+    public sealed class BooleanToTextConverterExtension : IMarkupExtension<BooleanToObjectConverter<string>>
+    {
+        public string True { get; set; }
+
+        public string False { get; set; }
+
+        public BooleanToObjectConverter<string> ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new BooleanToObjectConverter<string> { TrueValue = True, FalseValue = False };
+        }
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+        {
+            return ProvideValue(serviceProvider);
+        }
+    }
+}
