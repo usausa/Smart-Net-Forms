a138d1140ff4becaf2c68d691de825ef7c0d3bd2
usausa <machi.pon@gmail.com>
2021-02-14 19:07:31 +0900

    Update namespace setting

--- Smart.Forms/Assembly.cs
+++ Smart.Forms/Assembly.cs
@@ -1,17 +1,3 @@
 using System;
-using Xamarin.Forms;
 
 [assembly: CLSCompliant(false)]
-
-[assembly: XmlnsDefinition("http://schemas.smart/forms", "Smart.Forms")]
-[assembly: XmlnsDefinition("http://schemas.smart/forms", "Smart.Forms.Animations")]
-[assembly: XmlnsDefinition("http://schemas.smart/forms", "Smart.Forms.Components")]
-[assembly: XmlnsDefinition("http://schemas.smart/forms", "Smart.Forms.Data")]
-[assembly: XmlnsDefinition("http://schemas.smart/forms", "Smart.Forms.Expressions")]
-[assembly: XmlnsDefinition("http://schemas.smart/forms", "Smart.Forms.Input")]
-[assembly: XmlnsDefinition("http://schemas.smart/forms", "Smart.Forms.Interactivity")]
-[assembly: XmlnsDefinition("http://schemas.smart/forms", "Smart.Forms.Markup")]
-[assembly: XmlnsDefinition("http://schemas.smart/forms", "Smart.Forms.Messaging")]
-[assembly: XmlnsDefinition("http://schemas.smart/forms", "Smart.Forms.Resolver")]
-[assembly: XmlnsDefinition("http://schemas.smart/forms", "Smart.Forms.Validation")]
-[assembly: XmlnsDefinition("http://schemas.smart/forms", "Smart.Forms.ViewModels")]
