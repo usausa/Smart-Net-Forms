e82ef737dc866f9861e31d2e3e6130bf9c6a98ba
usausa <machi.pon@gmail.com>
2024-04-18 12:08:05 +0900

    Update state

--- Smart.Forms/Forms/ViewModels/BusyState.cs
+++ Smart.Forms/Forms/ViewModels/BusyState.cs
@@ -2,7 +2,7 @@ namespace Smart.Forms.ViewModels;
 
 using Smart.ComponentModel;
 
-public sealed class BusyState : NotificationObject, IBusyState
+public class BusyState : NotificationObject, IBusyState
 {
     private int counter;
 
